2412cd2f75a66984e2fde93e9bc0bf6ff0bc49d9
deep-sarkar <deepsarkar96@gmail.com>
2020-08-19 21:57:00 +0530

    [Deep]Add.added registration component

--- fundoo/src/app/components/registration/registration.component.ts
+++ fundoo/src/app/components/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -8,6 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class RegistrationComponent implements OnInit {
 
   constructor() { }
+  name = new FormControl('');
+  firstName = new FormControl('');
+  lastName = new FormControl('');
+  username = new FormControl('');
+  email = new FormControl('');
+  password = new FormControl('');
+  confirm = new FormControl('');
+
 
   ngOnInit() {
   }
