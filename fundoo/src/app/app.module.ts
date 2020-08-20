2d3d6332997e9c32f755f7b59254442301f5aa8c
deep-sarkar <deepsarkar96@gmail.com>
2020-08-20 11:00:28 +0530

    [Deep]Add.added login component in module

--- fundoo/src/app/app.module.ts
+++ fundoo/src/app/app.module.ts
@@ -9,12 +9,15 @@ import { RegistrationComponent } from './components/registration/registration.co
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentsComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +26,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     MatFormFieldModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
