da98a01f7ca56bfbe2d9990df2d4cec54588900d
deep-sarkar <deepsarkar96@gmail.com>
2020-08-20 22:33:29 +0530

    [Deep]Refactor.modified checked validation for username and password field

--- fundoo/src/app/components/login/login.component.ts
+++ fundoo/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +10,25 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
-  username = new FormControl('')
-  password = new FormControl('')
+  username = new FormControl('',[
+    Validators.required,
+    Validators.pattern(new RegExp("^[a-zA-Z0-9]{4,}$"))
 
+  ])
+  password = new FormControl('',[
+    Validators.required,
+    Validators.pattern(new RegExp("^[a-zA-Z0-9]{8,}$"))
+  ])
+
+  usernameErrorMsg(){
+    return this.username.hasError('required')?"enter username":
+    this.username.hasError('pattern')?"username must have atlest 4 digit, alphabet and numbers accepted only":"";
+  }
+
+  passwordErrorMsg(){
+    return this.password.hasError('required')?"enter password":
+    this.password.hasError('pattern')?"min. 8 digit alphanumeric only":"";
+  }
   ngOnInit() {
   }
 
