ce8b285296918b02280b6bf8fa6bd6b6e53705c4
deep-sarkar <deepsarkar96@gmail.com>
2020-08-20 19:17:22 +0530

    [Deep]Refactor.modified added validation for all the fields in form

--- fundoo/src/app/components/registration/registration.component.ts
+++ fundoo/src/app/components/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -8,14 +8,78 @@ import { FormControl } from '@angular/forms';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
-  first_name = new FormControl('');
-  last_name = new FormControl('');
-  username = new FormControl('');
-  email = new FormControl('');
-  password = new FormControl('');
-  confirm_password = new FormControl('');
+  first_name = new FormControl('',
+  [
+    Validators.required,
+    Validators.pattern(new RegExp("^[a-zA-Z]{3,}$"))
+  ]);
 
+  last_name = new FormControl('',
+  [
+    Validators.required,
+    Validators.pattern(new RegExp("^[a-zA-Z]{3,}$"))
+  ]);
+
+  username = new FormControl('',
+  [
+    Validators.required,
+    Validators.pattern(new RegExp("^[a-zA-Z0-9]{4,}$"))
+  ]);
+
+  email = new FormControl('',
+  [
+    Validators.required,
+    Validators.email
+  ]);
+
+  password = new FormControl('',
+  [
+    Validators.required,
+    Validators.pattern(new RegExp("^[a-zA-Z0-9]{8,}$"))
+  ]);
+
+  confirm_password = new FormControl('',
+  [
+    Validators.required
+  ]);
+
+  firstNameErrorMsg(){
+    return this.first_name.hasError('required')?"enter first name":
+    this.first_name.hasError('pattern')?"atlest 3 charecters & alphabet only":"";
+  }
+
+  lastNameErrorMsg(){
+    return this.last_name.hasError('required')?"enter last name":
+    this.last_name.hasError('pattern')?"atlest 3 charecters & alphabet only":"";
+  }
+
+  usernameErrorMsg(){
+    return this.username.hasError('required')?"enter username":
+    this.username.hasError('pattern')?"username must have atlest 4 digit, alphabet and numbers accepted only":"";
+  }
+
+  emailErrorMsg(){
+    return this.email.hasError('required')?"enter email":
+    this.email.hasError('email')?"please enter a valid email":
+    "verify email after registeration";
+  }
+
+  passwordErrorMsg(){
+    return this.password.hasError('required')?"enter password":
+    this.password.hasError('pattern')?"min. 8 digit alphanumeric only":"";
+  }
+
+  confirm_passwordErrorMsg(){
+    return this.password.hasError('required')?"please confirm password":""
+  }
+
+  passwordMatchError(){
+    if (this.password.value != this.confirm_password.value){
+      alert("Password didn't matched ")
+    }
+  }
+
+  constructor() { }
 
   ngOnInit() {
   }
