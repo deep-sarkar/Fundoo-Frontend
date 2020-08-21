3952b0206eeeeb7c4028655f04d7c4075240d74f
deep-sarkar <deepsarkar96@gmail.com>
2020-08-21 11:58:56 +0530

    [Deep]Refactor.modified added send mail service if email is valid

--- fundoo/src/app/components/forgot-password/forgot-password.component.ts
+++ fundoo/src/app/components/forgot-password/forgot-password.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { error } from 'console';
+import { ForgotPasswordService } from 'src/app/services/accountServices/forgot-password.service';
+import { RegisterService } from 'src/app/services/accountServices/register.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -13,13 +16,28 @@ export class ForgotPasswordComponent implements OnInit {
     Validators.email
   ])
 
+  recoveryMail:object;
+
   emailErrorMsg(){
     return this.email.hasError('required')?"Please enter email for recovery.":
       this.email.hasError('email')?"enter valid email.":"please check your email after submit"
   }
 
+  constructor(private sendmail:ForgotPasswordService) { }
 
-  constructor() { }
+  sendMail(){
+    this.recoveryMail = {email:this.email.value};
+    return this.sendmail.sendRecoveryMail(this.recoveryMail).subscribe(
+      response => {
+        console.log(response);
+        alert(response['msg']);
+      },
+      error =>{
+        console.log("error",error)
+      }
+    )
+  }
+
 
   ngOnInit() {
   }
