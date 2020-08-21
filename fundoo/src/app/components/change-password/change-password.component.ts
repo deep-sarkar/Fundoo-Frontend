f69c15c5639f64462f648a6dd5dc5701a839edf2
deep-sarkar <deepsarkar96@gmail.com>
2020-08-21 13:09:01 +0530

    [Deep]Add.added validation for form fields

--- fundoo/src/app/components/change-password/change-password.component.ts
+++ fundoo/src/app/components/change-password/change-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ValidateFormFieldService } from 'src/app/services/validationService/validate-form-field.service';
 
 @Component({
   selector: 'app-change-password',
@@ -7,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _validationService:ValidateFormFieldService) { }
+
+  password = new FormControl('',
+  [
+    Validators.required,
+    Validators.pattern(new RegExp("^[a-zA-Z0-9]{8,}$"))
+  ]);
+
+  confirm_password = new FormControl('',
+  [
+    Validators.required
+  ]);
+
+  passwordErrorMsg(){
+    return this._validationService.passwordErrorMsg(this.password)
+  }
+
+  confirm_passwordErrorMsg(){
+    return this._validationService.confirm_passwordErrorMsg(this.confirm_password)
+  }
 
   ngOnInit() {
   }
