24805ccbbd36900910fef2d6010111ca95ad7507
deep-sarkar <deepsarkar96@gmail.com>
2020-08-21 12:34:22 +0530

    [Deep]Add.added validate-form-field service to validate form field

---
+++ fundoo/src/app/services/validationService/validate-form-field.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidateFormFieldService {
+
+  constructor() { }
+
+  firstNameErrorMsg(first_name){
+    return first_name.hasError('required')?"enter first name":
+    first_name.hasError('pattern')?"atlest 3 charecters & alphabet only":"";
+  }
+
+  lastNameErrorMsg(last_name){
+    return last_name.hasError('required')?"enter last name":
+    last_name.hasError('pattern')?"atlest 3 charecters & alphabet only":"";
+  }
+
+  usernameErrorMsg(username){
+    return username.hasError('required')?"enter username":
+    username.hasError('pattern')?"username must have atlest 4 digit, alphabet and numbers accepted only":"";
+  }
+
+  emailErrorMsg(email){
+    return email.hasError('required')?"enter email":
+    email.hasError('email')?"please enter a valid email":
+    "verify email after registeration";
+  }
+
+  passwordErrorMsg(password){
+    return password.hasError('required')?"enter password":
+    password.hasError('pattern')?"min. 8 digit alphanumeric only":"";
+  }
+
+  confirm_passwordErrorMsg(confirm_password){
+    return confirm_password.hasError('required')?"please confirm password":""
+  }
+}
