29f4f3cb535c8d078c44973bc6f6a81ca9b3fe2e
deep-sarkar <deepsarkar96@gmail.com>
2020-08-21 12:33:33 +0530

    [Deep]Refactor.modified form field validation methods are injected from validate-form-field service

--- fundoo/src/app/components/registration/registration.component.ts
+++ fundoo/src/app/components/registration/registration.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { RegisterService } from 'src/app/services/accountServices/register.service';
+import { ValidateFormFieldService } from 'src/app/services/validationService/validate-form-field.service';
 
 @Component({
   selector: 'app-registration',
@@ -45,37 +46,31 @@ export class RegistrationComponent implements OnInit {
   ]);
   userData:object;
   
+  constructor(private register: RegisterService, private _validationService:ValidateFormFieldService) { }
+
   firstNameErrorMsg(){
-    return this.first_name.hasError('required')?"enter first name":
-    this.first_name.hasError('pattern')?"atlest 3 charecters & alphabet only":"";
+    return this._validationService.firstNameErrorMsg(this.first_name)
   }
 
   lastNameErrorMsg(){
-    return this.last_name.hasError('required')?"enter last name":
-    this.last_name.hasError('pattern')?"atlest 3 charecters & alphabet only":"";
+    return this._validationService.lastNameErrorMsg(this.last_name)
   }
 
   usernameErrorMsg(){
-    return this.username.hasError('required')?"enter username":
-    this.username.hasError('pattern')?"username must have atlest 4 digit, alphabet and numbers accepted only":"";
+    return this._validationService.usernameErrorMsg(this.username)
   }
 
   emailErrorMsg(){
-    return this.email.hasError('required')?"enter email":
-    this.email.hasError('email')?"please enter a valid email":
-    "verify email after registeration";
+    return this._validationService.emailErrorMsg(this.email)
   }
 
   passwordErrorMsg(){
-    return this.password.hasError('required')?"enter password":
-    this.password.hasError('pattern')?"min. 8 digit alphanumeric only":"";
+    return this._validationService.passwordErrorMsg(this.password)
   }
 
   confirm_passwordErrorMsg(){
-    return this.password.hasError('required')?"please confirm password":""
+    return this._validationService.confirm_passwordErrorMsg(this.confirm_password)
   }
-
-  constructor(private register: RegisterService) { }
 
   registerNewUser(){
     if (this.password.value != this.confirm_password.value){
