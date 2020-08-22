45d7487ad77f581ba767508e557aa8f4ccc4a41f
deep-sarkar <deepsarkar96@gmail.com>
2020-08-22 12:29:37 +0530

    [Deep]Refactor.modified post api call with authorization detail

--- fundoo/src/app/services/accountServices/account-http.service.ts
+++ fundoo/src/app/services/accountServices/account-http.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,23 +9,35 @@ export class AccountHttpService {
 
   constructor(private _http:HttpClient) { }
   
+  baseUrl = environment.accountUrl
+
+  token = localStorage.getItem('token')
+
   changePassword(data){
-    const headers = new HttpHeaders().set('content-type', 'application/json');
-    return this._http.post("http://127.0.0.1:8000/accounts/change_password/", data)
+    console.log("token",this.token)
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': "JWT"+this.token
+    })
+
+    return this._http.post(this.baseUrl + "change_password/", data, {headers:{
+      "Content-Type": "application/json",
+      "Authorization": "JWT "+this.token,
+    }})
   }
 
   sendRecoveryMail(email){
     const headers = new HttpHeaders().set('content-type', 'application/json');
-    return this._http.post("http://127.0.0.1:8000/accounts/forgot_password/",email)
+    return this._http.post(this.baseUrl + "forgot_password/",email)
   }
 
   userLogin(data){
     const headers = new HttpHeaders().set('content-type', 'application/json');
-    return this._http.post("http://127.0.0.1:8000/accounts/login/", data)
+    return this._http.post(this.baseUrl + "login/", data)
   }
 
   registerNewUser(userData){
     const headers = new HttpHeaders().set('content-type', 'application/json');
-    return this._http.post("http://127.0.0.1:8000/accounts/register/", userData)
+    return this._http.post(this.baseUrl + "register/", userData)
   }
 }
