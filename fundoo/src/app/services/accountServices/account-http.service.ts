c618257b3211f29360a5c7ee493ecaac23552aa1
deep-sarkar <deepsarkar96@gmail.com>
2020-08-24 00:20:13 +0530

    [Deep]Add.added reset password service

--- fundoo/src/app/services/accountServices/account-http.service.ts
+++ fundoo/src/app/services/accountServices/account-http.service.ts
@@ -15,10 +15,6 @@ export class AccountHttpService {
 
   changePassword(data){
     console.log("token",this.token)
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': "JWT"+this.token
-    })
 
     return this._http.post(this.baseUrl + "change_password/", data, {headers:{
       "Content-Type": "application/json",
@@ -45,4 +41,14 @@ export class AccountHttpService {
     const headers = new HttpHeaders().set('content-type', 'application/json');
     return this._http.get(this.baseUrl + "activate/"+surl)
   }
+
+  checkUserExistance(surl){
+    return this._http.get(this.baseUrl+"check_user/"+surl)
+  }
+
+  resetPassword(surl, user_data){
+    console.log(this.baseUrl+"reset_paassword/"+surl,user_data)
+    return this._http.post(this.baseUrl+"reset_paassword/" ,user_data)
+  }
+
 }
