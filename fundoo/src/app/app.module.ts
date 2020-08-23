5b16152e4026045d3a7cde530640de0630cc71e6
deep-sarkar <deepsarkar96@gmail.com>
2020-08-23 15:33:07 +0530

    [Deep]Add.added MatNavToll and related imports to imports

--- fundoo/src/app/app.module.ts
+++ fundoo/src/app/app.module.ts
@@ -1,29 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ComponentsComponent } from './components/components.component';
-import { RegistrationComponent } from './components/registration/registration.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { LoginComponent } from './components/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// Component
+import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { MatCardModule } from '@angular/material';
 import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { LoginComponent } from './components/login/login.component';
+// Material
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+// services
 import { ValidateFormFieldService } from './services/validationService/validate-form-field.service';
 import { AccountHttpService } from './services/accountServices/account-http.service';
+
 
 const fundooRoutes: Routes=[
   {path:"register", component:RegistrationComponent},
   {path:"login", component:LoginComponent},
   {path:"forgot", component:ForgotPasswordComponent},
-  {path:"changePassword", component:ChangePasswordComponent}
+  {path:"changePassword", component:ChangePasswordComponent},
+  {path:"", component:SideNavComponent}
 ]
 
 
@@ -34,7 +42,8 @@ const fundooRoutes: Routes=[
     RegistrationComponent,
     LoginComponent,
     ForgotPasswordComponent,
-    ChangePasswordComponent
+    ChangePasswordComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
@@ -47,7 +56,10 @@ const fundooRoutes: Routes=[
     MatButtonModule,
     RouterModule.forRoot(fundooRoutes),
     HttpClientModule,
-    MatCardModule
+    MatCardModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [
     AccountHttpService,
