e1a23581ab772634a6082c3be75af46c398337b1
deep-sarkar <deepsarkar96@gmail.com>
2020-08-24 00:19:44 +0530

    [Deep]Add.added reset password component

---
+++ fundoo/src/app/components/reset-password/reset-password.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
+import { ActivatedRoute } from '@angular/router';
+import { error } from 'console';
+import { AccountHttpService } from 'src/app/services/accountServices/account-http.service';
+import { ValidateFormFieldService } from 'src/app/services/validationService/validate-form-field.service';
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.component.html',
+  styleUrls: ['./reset-password.component.scss']
+})
+export class ResetPasswordComponent implements OnInit {
+
+  constructor( 
+    private _httpService:AccountHttpService,
+    private _route:ActivatedRoute,
+    private _snakeBar:MatSnackBar,
+    private _validationService:ValidateFormFieldService,
+    ) { }
+
+    surl:string;
+    user:boolean=false;
+    data:object;
+
+    password = new FormControl('',
+  [
+    Validators.required,
+    Validators.pattern(new RegExp("^[a-zA-Z0-9]{8,}$"))
+  ]);
+
+  confirm_password = new FormControl('',
+  [
+    Validators.required
+  ]);
+
+  passwordErrorMsg(){
+    return this._validationService.passwordErrorMsg(this.password)
+  }
+
+  confirm_passwordErrorMsg(){
+    return this._validationService.confirm_passwordErrorMsg(this.confirm_password)
+  }
+
+  checkUser(){
+    this.surl = this._route.snapshot.paramMap.get('surl')
+    this._httpService.checkUserExistance(this.surl)
+    .subscribe(
+      response =>{
+        if(response['code']===203){
+          this.user=true;
+          console.log("response",response)
+        }
+      },
+      error =>{
+        console.log('error',error)
+      }
+    )
+  }
+
+  resetNewPassword(){
+    this.data = {
+      password:this.password.value,
+      confirm_password:this.confirm_password.value,
+      surl:this.surl
+    }
+    console.log(this.data)
+    console.log(this.surl)
+    this._httpService.resetPassword(this.surl, this.data)
+    .subscribe(
+      response =>{
+        this._snakeBar.open(response['msg'],'exit',{duration:5000})
+      },
+      error=>{
+        console.log("error",error)
+      }
+      )
+  }
+
+  ngOnInit() {
+    this.checkUser()
+  }
+
+}
