00da28155819d877a08a78cc7b261c94f2a55801
deep-sarkar <deepsarkar96@gmail.com>
2020-08-24 21:23:12 +0530

    [Deep]Add.added generic service for handeling http services

---
+++ fundoo/src/app/services/genericService/generic.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GenericService {
+
+  constructor(private _httpClint:HttpClient) { }
+
+  getService(url:string, header:object){
+    return this._httpClint.get(url, header)
+  }
+
+  postService(url:string, data:any, header:object){
+    return this._httpClint.post(url, data, header)
+  }
+
+}
