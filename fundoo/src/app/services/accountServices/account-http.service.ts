df583106f08ffa7b787bf5cb0f7a33e39cd38bd8
deep-sarkar <deepsarkar96@gmail.com>
2020-08-24 21:24:18 +0530

    [Deep]Refactor.modified imported and used customized http service from generic-service

--- fundoo/src/app/services/accountServices/account-http.service.ts
+++ fundoo/src/app/services/accountServices/account-http.service.ts
@@ -1,54 +1,70 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { GenericService } from '../genericService/generic.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountHttpService {
 
-  constructor(private _http:HttpClient) { }
+  constructor(private _http:GenericService) { }
   
   baseUrl = environment.accountUrl
 
   token = localStorage.getItem('token')
 
-  changePassword(data){
+  changePassword(data:object){
     console.log("token",this.token)
-
-    return this._http.post(this.baseUrl + "change_password/", data, {headers:{
-      "Content-Type": "application/json",
-      "Authorization": "JWT "+this.token,
-    }})
+    let headers = new HttpHeaders(
+      {"Content-Type": "application/json",
+      "Authorization": "JWT "+this.token})
+    
+    return this._http.postService(this.baseUrl + "change_password/", data, {headers:headers})
   }
 
-  sendRecoveryMail(email){
-    const headers = new HttpHeaders().set('content-type', 'application/json');
-    return this._http.post(this.baseUrl + "forgot_password/",email)
+  sendRecoveryMail(email:object){
+    let headers = new HttpHeaders(
+      {"Content-Type": "application/json"}
+      )
+
+    return this._http.postService(this.baseUrl + "forgot_password/",email,{headers:headers})
   }
 
-  userLogin(data){
-    const headers = new HttpHeaders().set('content-type', 'application/json');
-    return this._http.post(this.baseUrl + "login/", data)
+  userLogin(data:object){
+    let headers = new HttpHeaders(
+      {"Content-Type": "application/json"}
+      )
+      
+    return this._http.postService(this.baseUrl + "login/", data, {headers:headers})
   }
 
-  registerNewUser(userData){
-    const headers = new HttpHeaders().set('content-type', 'application/json');
-    return this._http.post(this.baseUrl + "register/", userData)
+  registerNewUser(userData:object){
+    let headers = new HttpHeaders(
+      {"Content-Type": "application/json"}
+      )
+    return this._http.postService(this.baseUrl + "register/", userData,{headers:headers})
   }
 
-  activateAccount(surl){
-    const headers = new HttpHeaders().set('content-type', 'application/json');
-    return this._http.get(this.baseUrl + "activate/"+surl)
+  activateAccount(surl:string){
+    let headers = new HttpHeaders(
+      {"Content-Type": "application/json"}
+      )
+    return this._http.getService(this.baseUrl + "activate/" + surl, {headers:headers})
   }
 
-  checkUserExistance(surl){
-    return this._http.get(this.baseUrl+"check_user/"+surl)
+  checkUserExistance(surl:string){
+    let headers = new HttpHeaders(
+      {"Content-Type": "application/json"}
+    )
+    return this._http.getService(this.baseUrl+"check_user/" + surl, {headers:headers})
   }
 
-  resetPassword(surl, user_data){
-    console.log(this.baseUrl+"reset_paassword/"+surl,user_data)
-    return this._http.post(this.baseUrl+"reset_paassword/" ,user_data)
+  resetPassword(user_data:object){
+    let headers = new HttpHeaders(
+      {"Content-Type": "application/json"}
+    )
+    return this._http.postService(this.baseUrl+"reset_paassword/" , user_data, {headers:headers})
   }
 
 }
