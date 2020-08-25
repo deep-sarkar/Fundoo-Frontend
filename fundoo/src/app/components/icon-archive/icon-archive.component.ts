1c7d11e39d0e4aa564cd79fa660b661426d771a4
deep-sarkar <deepsarkar96@gmail.com>
2020-08-25 17:44:12 +0530

    [Deep]Add.added archive icon component

---
+++ fundoo/src/app/components/icon-archive/icon-archive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-archive',
+  templateUrl: './icon-archive.component.html',
+  styleUrls: ['./icon-archive.component.scss']
+})
+export class IconArchiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
