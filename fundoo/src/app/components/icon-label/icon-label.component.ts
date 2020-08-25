c5195806196de40bfc0fcbb360dfadd711ed5626
deep-sarkar <deepsarkar96@gmail.com>
2020-08-25 17:43:22 +0530

    [Deep]Add.added label component

---
+++ fundoo/src/app/components/icon-label/icon-label.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-label',
+  templateUrl: './icon-label.component.html',
+  styleUrls: ['./icon-label.component.scss']
+})
+export class IconLabelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
