3b1edc9c5a3e7b33858b56ae9c52e5506cf6ada3
deep-sarkar <deepsarkar96@gmail.com>
2020-08-25 17:42:49 +0530

    [Deep]Add.added dashbord component

--- fundoo/src/app/app.module.ts
+++ fundoo/src/app/app.module.ts
@@ -6,17 +6,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TextFieldModule } from '@angular/cdk/text-field';
-// Component
-import { AppComponent } from './app.component';
-import { ComponentsComponent } from './components/components.component';
-import { RegistrationComponent } from './components/registration/registration.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { ChangePasswordComponent } from './components/change-password/change-password.component';
-import { SideNavComponent } from './components/side-nav/side-nav.component';
-import { LoginComponent } from './components/login/login.component';
-import { ActivateAccountComponent } from './components/activate-account/activate-account.component';
-import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { CreateNoteComponent } from './components/create-note/create-note.component';
 
 // Material
 import { MatInputModule } from '@angular/material/input';
@@ -34,16 +23,38 @@ import { AccountHttpService } from './services/accountServices/account-http.serv
 import { UtilityService } from './services/utilityService/utility.service';
 import { GenericService } from './services/genericService/generic.service';
 
+//icon component
+import { IconLabelComponent } from './components/icon-label/icon-label.component';
+import { IconPaletteComponent } from './components/icon-palette/icon-palette.component';
+import { IconInsertPhotoComponent } from './components/icon-insert-photo/icon-insert-photo.component';
+import { IconArchiveComponent } from './components/icon-archive/icon-archive.component';
+import { IconPinComponent } from './components/icon-pin/icon-pin.component';
+import { IconReminderComponent } from './components/icon-reminder/icon-reminder.component';
+import { IconCollaboratorComponent } from './components/icon-collaborator/icon-collaborator.component';
+
+// Component
+import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import { LoginComponent } from './components/login/login.component';
+import { ActivateAccountComponent } from './components/activate-account/activate-account.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { CreateNoteComponent } from './components/create-note/create-note.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+
+
 
 const fundooRoutes: Routes=[
   {path:"register", component:RegistrationComponent},
   {path:"login", component:LoginComponent},
   {path:"forgot", component:ForgotPasswordComponent},
   {path:"changePassword", component:ChangePasswordComponent},
-  {path:"nav", component:SideNavComponent},
   {path:"activate/:surl", component:ActivateAccountComponent},
   {path:"resetPassword/:surl", component:ResetPasswordComponent},
-  {path:"", component:CreateNoteComponent}
+  {path:"", component:DashboardComponent},
 ]
 
 
@@ -55,10 +66,17 @@ const fundooRoutes: Routes=[
     LoginComponent,
     ForgotPasswordComponent,
     ChangePasswordComponent,
-    SideNavComponent,
     ActivateAccountComponent,
     ResetPasswordComponent,
-    CreateNoteComponent
+    CreateNoteComponent,
+    IconLabelComponent,
+    IconPaletteComponent,
+    IconInsertPhotoComponent,
+    IconArchiveComponent,
+    IconPinComponent,
+    IconReminderComponent,
+    IconCollaboratorComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
