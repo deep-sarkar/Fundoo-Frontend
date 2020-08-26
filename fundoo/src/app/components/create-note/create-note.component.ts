63f69c2295de6f7d14faabfe30a6241dd5ccdd77
deep-sarkar <deepsarkar96@gmail.com>
2020-08-26 20:05:18 +0530

    [Deep]Add.added create function if note title and note field is not none

--- fundoo/src/app/components/create-note/create-note.component.ts
+++ fundoo/src/app/components/create-note/create-note.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { AccountHttpService } from 'src/app/services/accountServices/account-http.service';
+import { UtilityService } from 'src/app/services/utilityService/utility.service';
 
 @Component({
   selector: 'app-create-note',
@@ -7,18 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateNoteComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _httpService:AccountHttpService,
+    private _snackBar:UtilityService
+  ) { }
 
   cardOpen:boolean=false;
+  noteData:object;
+  title = new FormControl('');
+  noteBody = new FormControl('');
+  
+  create(){
+    if(this.title.value && this.noteBody.value){
+      this.noteData = {
+        title:this.title.value,
+        note:this.noteBody.value
+      }
+      this._httpService.createNotes(this.noteData)
+      .subscribe(
+        response => {
+          if (response['code'] == 201){
+            this._snackBar.snackBarMessage("New note created !!!")
+            this.title= new FormControl('')
+            this.noteBody = new FormControl('')
+          }else{
+            this._snackBar.snackBarMessage(response['msg'])
+          }
+        },
+        error =>{
+            console.log("error",error)
+        }
+      )
+    }
+  }
+  
 
   cardOpenClose(){
     if(this.cardOpen==false){
       this.cardOpen=true
     }else{
+      this.create()
       this.cardOpen=false
     }
   }
 
+  
 
   ngOnInit() {
   }
