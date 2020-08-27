7ebe8a254dfaf955ff6f9bb9ee8b7cb49e3c269b
deep-sarkar <deepsarkar96@gmail.com>
2020-08-27 17:40:22 +0530

    [Deep]Add.added app routes

--- fundoo/src/app/app-routing.module.ts
+++ fundoo/src/app/app-routing.module.ts
@@ -1,11 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ActivateAccountComponent } from './components/activate-account/activate-account.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import { CreateNoteComponent } from './components/create-note/create-note.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"register", component:RegistrationComponent},
+    {path:"login", component:LoginComponent},
+    {path:"forgot", component:ForgotPasswordComponent},
+    {path:"changePassword", component:ChangePasswordComponent},
+    {path:"activate/:surl", component:ActivateAccountComponent},
+    {path:"resetPassword/:surl", component:ResetPasswordComponent},
+    {
+      path:"", component:DashboardComponent,
+      children:[
+        {path:"", component:NotesComponent,
+        children:[
+          {path:"create", component:CreateNoteComponent}
+        ]
+      },
+    ]
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
