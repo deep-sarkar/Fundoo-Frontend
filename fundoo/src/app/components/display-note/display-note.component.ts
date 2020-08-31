e8262cad23af273e9eb6db165fac22e9d2c2f67e
deep-sarkar <deepsarkar96@gmail.com>
2020-08-31 11:51:08 +0530

    [Deep]Add.added functionality for pin note icon

--- fundoo/src/app/components/display-note/display-note.component.ts
+++ fundoo/src/app/components/display-note/display-note.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { SingleNoteComponent } from '../single-note/single-note.component';
 
 @Component({
   selector: 'app-display-note',
@@ -7,9 +9,26 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class DisplayNoteComponent implements OnInit {
 
-  constructor( ) { }
+  constructor(private _dialogue:MatDialog ) { }
 
   @Input() allNotes:object[];
+  isPin:boolean;
+  checkPin:any;
+
+  getPin(pin:boolean){
+    this.isPin=pin
+    return true
+  }
+
+
+  openDialogue(id:number){
+    console.log(id)
+    this._dialogue.open(SingleNoteComponent,{
+      width:'50%',
+      height:"80%",
+      panelClass: 'dialog-content'
+    })
+  }
 
   ngOnInit() {
   }
