06f9de7224756aac4df90e98b3d3c2a8ef5bd271
deep-sarkar <deepsarkar96@gmail.com>
2020-09-01 10:59:31 +0530

    [Deep]Add.added add reminder functionality

--- fundoo/src/app/components/display-note/display-note.component.ts
+++ fundoo/src/app/components/display-note/display-note.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material';
+import { AccountHttpService } from 'src/app/services/accountServices/account-http.service';
+import { UtilityService } from 'src/app/services/utilityService/utility.service';
+import { NotesComponent } from '../notes/notes.component';
 import { SingleNoteComponent } from '../single-note/single-note.component';
 
 @Component({
@@ -9,17 +12,68 @@ import { SingleNoteComponent } from '../single-note/single-note.component';
 })
 export class DisplayNoteComponent implements OnInit {
 
-  constructor(private _dialogue:MatDialog ) { }
+  constructor(
+        private _dialogue:MatDialog,
+        private _accountService:AccountHttpService,
+        private _utility:UtilityService
+     ) { }
 
   @Input() allNotes:object[];
-  isPin:boolean;
-  checkPin:any;
+  @Output() update = new EventEmitter<boolean>()
+  isPin:boolean=false;
+  reminder:string=null;
+  id:number;
+  singleNote:object={reminder:null};
+
 
   getPin(pin:boolean){
     this.isPin=pin
     return true
   }
 
+  getId(noteId:number){
+    console.log(noteId)
+    this.id=noteId
+    this._accountService.getSingleNote(this.id)
+    .subscribe(
+      response =>{
+        this.singleNote=response["data"]
+        // console.log(this.singleNote)
+      },
+      error =>{
+        console.log("error",error)
+      }
+    )
+  }
+
+  setReminder($event){
+    console.log($event)
+    let validation=this._utility.validateReminder($event)
+    if (validation){
+      this.singleNote['reminder']=$event
+      // console.log(this.singleNote)
+      this._accountService.updateSingleNote(this.id,this.singleNote)
+      .subscribe(
+        response =>{
+          if(response['code']==202){
+          this.updateTrigger()
+          this._utility.snackBarMessage("Note updated !!")
+          }else{
+            this._utility.snackBarMessage(response['msg'])
+          }
+        },
+        error =>{
+            console.log("error",error)
+        }
+      )
+    }
+
+  }
+
+  updateTrigger(){
+    this.update.emit(true)
+    // console.log("Emmitted")
+  }
 
   openDialogue(id:number){
     console.log(id)
@@ -29,6 +83,7 @@ export class DisplayNoteComponent implements OnInit {
       panelClass: 'dialog-content'
     })
   }
+
 
   ngOnInit() {
   }
