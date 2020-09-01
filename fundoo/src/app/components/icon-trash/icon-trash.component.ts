838b52681fb3528d2fd80f8ebe3506e65f36a477
deep-sarkar <deepsarkar96@gmail.com>
2020-09-01 15:58:07 +0530

    [Deep]Add.added trash icon

---
+++ fundoo/src/app/components/icon-trash/icon-trash.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-trash',
+  templateUrl: './icon-trash.component.html',
+  styleUrls: ['./icon-trash.component.scss']
+})
+export class IconTrashComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
