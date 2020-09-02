2e603cd9d7a1ef20013c7090d4ab1e387a63fa07
deep-sarkar <deepsarkar96@gmail.com>
2020-09-02 12:17:29 +0530

    [Deep]Add.added structure and style of single note view

--- fundoo/src/app/components/single-note/single-note.component.ts
+++ fundoo/src/app/components/single-note/single-note.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-single-note',
@@ -8,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class SingleNoteComponent implements OnInit {
 
   constructor() { }
+
+  title = new FormControl()
+  noteBody = new FormControl()
 
   ngOnInit() {
   }
