9ab7a5a90b93a62446f029cd8f34903fb132157e
deep-sarkar <deepsarkar96@gmail.com>
2020-09-03 13:16:55 +0530

    [Deep]Add.added create label function

--- fundoo/src/app/components/label/label.component.ts
+++ fundoo/src/app/components/label/label.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { DataService } from 'src/app/services/dataService/data.service';
+import { UtilityService } from 'src/app/services/utilityService/utility.service';
 
 @Component({
   selector: 'app-label',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LabelComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _dataService:DataService,
+    private _utility:UtilityService
+  ) { }
+
+  newLabel = new FormControl('')
+  
+  createLabel(){
+    let labelData ={
+      label:this.newLabel.value
+    }
+    this._dataService.createLabel(labelData)
+    .subscribe(
+      response =>{
+        if(response['code']==201){
+          this._utility.snackBarMessage("New label created")
+        }else{
+          this._utility.snackBarMessage(response["msg"])
+        }
+      },
+      error =>{
+        console.log("error",error)
+      }
+    )
+  }
+
 
   ngOnInit() {
   }
-
 }
