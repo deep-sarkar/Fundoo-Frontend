6b55ce2dc90de52388936328e07f4a9db4bc3551
deep-sarkar <deepsarkar96@gmail.com>
2020-09-03 12:57:58 +0530

    [Deep]Add.added data services for note operations

---
+++ fundoo/src/app/services/dataService/data.service.ts
@@ -0,0 +1,60 @@
+import { HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { GenericService } from '../genericService/generic.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(
+    private _http:GenericService
+  ) { }
+
+  token = localStorage.getItem('token')
+  noteUrl = environment.notesUrl
+
+  getNotes(): Observable<any>{
+    let headers = new HttpHeaders(
+      {"Content-Type": "application/json",
+      "Authorization": "JWT "+this.token})
+    return this._http.getService(this.noteUrl + "create/", {headers:headers})
+  }
+
+  createNotes(noteData:object): Observable<any>{
+    let headers = new HttpHeaders(
+      {"Content-Type": "application/json",
+      "Authorization": "JWT "+this.token})
+    return this._http.postService(this.noteUrl + "create/", noteData,{headers:headers})
+  }
+
+  getSingleNote(id:number): Observable<any>{
+    let headers = new HttpHeaders(
+      {"Content-Type": "application/json",
+      "Authorization": "JWT "+this.token})
+      return this._http.getService(this.noteUrl+"open/"+id+"/",{headers:headers})
+  }
+  
+  updateSingleNote(id:number, noteData:object): Observable<any>{
+    let headers = new HttpHeaders(
+      {"Content-Type": "application/json",
+      "Authorization": "JWT "+this.token})
+      return this._http.putService(this.noteUrl+"open/"+id+"/",noteData,{headers:headers})
+  }
+
+  createLabel(labelData:object): Observable<any>{
+    let headers = new HttpHeaders(
+      {"Content-Type": "application/json",
+      "Authorization": "JWT "+this.token})
+    return this._http.postService(this.noteUrl + "create/label/", labelData,{headers:headers})
+  }
+
+  getLabel(): Observable<any>{
+    let headers = new HttpHeaders(
+      {"Content-Type": "application/json",
+      "Authorization": "JWT "+this.token})
+    return this._http.getService(this.noteUrl + "create/label/", {headers:headers})
+  }
+}
