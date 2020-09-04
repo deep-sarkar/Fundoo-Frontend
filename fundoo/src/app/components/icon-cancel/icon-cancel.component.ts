627ac03d044cdb5ffafbdf29806619b50ea811d9
deep-sarkar <deepsarkar96@gmail.com>
2020-09-04 18:59:33 +0530

    [Deep]Add.added cancel icon

---
+++ fundoo/src/app/components/icon-cancel/icon-cancel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-cancel',
+  templateUrl: './icon-cancel.component.html',
+  styleUrls: ['./icon-cancel.component.scss']
+})
+export class IconCancelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
