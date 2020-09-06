8b4f5f26216604c302ad688ebb78bc543c2ddd3a
deep-sarkar <deepsarkar96@gmail.com>
2020-09-06 10:29:27 +0530

    [Deep]Add.added reminder component to display reminder notes

---
+++ fundoo/src/app/components/reminder/reminder.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/dataService/data.service';
+
+@Component({
+  selector: 'app-reminder',
+  templateUrl: './reminder.component.html',
+  styleUrls: ['./reminder.component.scss']
+})
+export class ReminderComponent implements OnInit {
+
+  constructor(
+    private _dataService:DataService
+    ) { }
+
+  allNotes:object;
+  
+  allReminderNotes(){
+    this._dataService.getReminder()
+    .subscribe(
+      result =>{
+        if(result['code']==200){
+          this.allNotes=result['data']
+        }else{
+          console.log(result)
+        }
+      },
+      error =>{
+        console.log("error",error)
+      }
+    )
+  }
+
+  ngOnInit() {
+    this.allReminderNotes()
+  }
+
+}
