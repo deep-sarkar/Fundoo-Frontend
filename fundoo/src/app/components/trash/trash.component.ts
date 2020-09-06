c593d402dd1d6dc5492fe37993666cb96b4ac5ff
deep-sarkar <deepsarkar96@gmail.com>
2020-09-06 15:45:43 +0530

    [Deep]Add.added untrash icon and fucntion

---
+++ fundoo/src/app/components/trash/trash.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/dataService/data.service';
+
+@Component({
+  selector: 'app-trash',
+  templateUrl: './trash.component.html',
+  styleUrls: ['./trash.component.scss']
+})
+export class TrashComponent implements OnInit {
+
+  constructor(
+    private _dataService:DataService
+  ) { }
+  
+  allNotes:object;
+
+
+  getAllTrashNote(){
+    this._dataService.getTrash()
+    .subscribe(
+      respones =>{
+        if(respones['code']===200){
+          // console.log(respones["data"])
+          this.allNotes = respones["data"]
+        }
+      },
+      error =>{
+        console.log("error",error)
+      }
+    )
+  }
+
+  ngOnInit() {
+    this.getAllTrashNote()
+  }
+
+}
