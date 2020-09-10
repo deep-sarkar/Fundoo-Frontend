13e77629a1d8bf33390f7859fd255188fbb6f8b9
deep-sarkar <deepsarkar96@gmail.com>
2020-09-10 21:40:24 +0530

    [Deep]Add.added searh component

---
+++ fundoo/src/app/components/search/search.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { UtilityService } from 'src/app/services/utilityService/utility.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+
+  userInput:string;
+
+  constructor(
+    private _utility:UtilityService
+  ) { }
+
+  getInput(){
+    console.log("input",this.userInput)
+    this._utility.searchItem.next(this.userInput)
+  }
+
+  ngOnInit() {
+
+  }
+
+}
