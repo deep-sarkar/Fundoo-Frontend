abec234956b9fa2412addcb6254eee0952f22e4f
deep-sarkar <deepsarkar96@gmail.com>
2020-09-10 12:13:33 +0530

    [Deep]Add.added change password redirect button

--- fundoo/src/app/components/home/home.component.ts
+++ fundoo/src/app/components/home/home.component.ts
@@ -1,6 +1,9 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import jwt_decode from 'jwt-decode'
+import { AccountHttpService } from 'src/app/services/accountServices/account-http.service';
+import { UtilityService } from 'src/app/services/utilityService/utility.service';
 
 @Component({
   selector: 'app-home',
@@ -10,12 +13,14 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
 
   constructor(
-    private _route:Router
+    private _route:Router,
+    private _accountService:AccountHttpService,
+    private _utility:UtilityService
     ) { }
     
   token = localStorage.getItem("token")
   
-  username = "deep"
+  username:string;
   
 
   register(){
@@ -25,9 +30,45 @@ export class HomeComponent implements OnInit {
   login(){
     this._route.navigate(['login'])
   }
+ 
+  notes(){
+    this._route.navigate([''])
+  }
+
+  changePassword(){
+    this._route.navigate(['changePassword'])
+    console.log(1111)
+  }
+
+  logout(){
+    this._accountService.logout()
+    .subscribe(
+      response => {
+        console.log(response)
+        if(response['code']===200){
+          localStorage.removeItem("token")
+          this._utility.snackBarMessage("Logout successfull")
+          this._route.navigate(["home"])
+          console.log(this.token)
+        }
+      },
+      error =>{
+        console.log("error",error)
+      }
+    )
+  }
+
+  getUsername(){
+    if(this.token != null){
+      let decode = jwt_decode(this.token)
+      // console.log(decode)
+      this.username = decode['username']
+    }
+  }
 
   ngOnInit() {
-
+    this.getUsername()
+    console.log(this.token)
   }
 
 }
