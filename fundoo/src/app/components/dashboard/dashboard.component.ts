de4d23180df4040559acb08c718115326fc1a6b0
deep-sarkar <deepsarkar96@gmail.com>
2020-09-11 18:20:52 +0530

    [Deep]Add.added style and div for different component alignment

--- fundoo/src/app/components/dashboard/dashboard.component.ts
+++ fundoo/src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { Router } from '@angular/router';
+import { AccountHttpService } from 'src/app/services/accountServices/account-http.service';
 import { UtilityService } from 'src/app/services/utilityService/utility.service';
 import { LabelComponent } from '../label/label.component';
+import jwt_decode from 'jwt-decode'
+
 
 @Component({
   selector: 'app-dashboard',
@@ -12,70 +15,82 @@ import { LabelComponent } from '../label/label.component';
 export class DashboardComponent implements OnInit {
 
   constructor(
-      private _router:Router,
-      private _dialog:MatDialog,
-      private _utility:UtilityService
-    ) {
-      _utility.viewClass
+    private _router: Router,
+    private _dialog: MatDialog,
+    private _utility: UtilityService,
+    private _accountService: AccountHttpService
+  ) {
+    _utility.viewClass
       .subscribe(
-        result =>{
-          if(result==="main-container-grid"){
+        result => {
+          if (result === "main-container-grid") {
             this.isGrid = true
-          }else{
+          } else {
             this.isGrid = false
           }
         }
       )
-     }
+  }
 
+  username: string = ''
 
-  isGrid:boolean=true;
+  isGrid: boolean = true;
 
-  gridView(){
+  getUsername() {
+    if (this._accountService.isLoggedIn()) {
+      let decode = jwt_decode(localStorage.getItem('token'))
+      let uname = decode['username']
+      this.username = uname.substr(0,2).toUpperCase()
+      // console.log(decode)
+    }
+  }
+
+  gridView() {
     this.isGrid = true
     this._utility.viewClass.next("main-container-grid")
   }
 
-  listView(){
+  listView() {
     this.isGrid = false
     this._utility.viewClass.next("main-container-list")
   }
 
-  home(){
+  home() {
     this._router.navigate(['home'])
   }
 
-  createNote(){
+  createNote() {
     this._utility.searchItem.next('')
     this._router.navigate(['create'])
   }
 
-  openLabel(){
-    let labelRef = this._dialog.open(LabelComponent,{
-      position:{top:"5%"},
-      minWidth:"20%",
-      minHeight:"10vh",
-      height:"auto",
-      maxHeight:"85vh",
-      disableClose:true,
-      panelClass:'label-dialog'
+  openLabel() {
+    let labelRef = this._dialog.open(LabelComponent, {
+      position: { top: "5%" },
+      minWidth: "20%",
+      minHeight: "10vh",
+      height: "auto",
+      maxHeight: "85vh",
+      disableClose: true,
+      panelClass: 'label-dialog'
     })
   }
-  
-  reminderNote(){
+
+  reminderNote() {
     this._router.navigate(['reminder'])
   }
- 
-  archivesNote(){
+
+  archivesNote() {
     this._router.navigate(['archives'])
   }
-  
-  trashNotes(){
+
+  trashNotes() {
     this._router.navigate(["trash"])
   }
-  
+
   ngOnInit() {
     this.createNote()
+    this.getUsername()
   }
 
 }
