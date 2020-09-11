81864972a893e5f9ee689c94ad8ac7d42dde7794
deep-sarkar <deepsarkar96@gmail.com>
2020-09-11 11:43:36 +0530

    [Deep]Add.added isLoggedIn() function to return true or false if user is loggedin

--- fundoo/src/app/services/accountServices/account-http.service.ts
+++ fundoo/src/app/services/accountServices/account-http.service.ts
@@ -89,7 +89,7 @@ export class AccountHttpService {
   }
 
   isLoggedIn(){
-    console.log(!!localStorage.getItem('token'))
+    // console.log(!!localStorage.getItem('token'))
     return !!localStorage.getItem('token')
   }
 
