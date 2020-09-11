bd7d38ece2ffba3a3401a543e20609224d7a70db
deep-sarkar <deepsarkar96@gmail.com>
2020-09-11 19:22:54 +0530

    [Deep]Add.added back button route to note component

--- fundoo/src/app/components/change-password/change-password.component.ts
+++ fundoo/src/app/components/change-password/change-password.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AccountHttpService } from 'src/app/services/accountServices/account-http.service';
 import { UtilityService } from 'src/app/services/utilityService/utility.service';
 import { ValidateFormFieldService } from 'src/app/services/validationService/validate-form-field.service';
@@ -16,7 +17,8 @@ export class ChangePasswordComponent implements OnInit {
   constructor(
     private _validationService:ValidateFormFieldService,
     private _httpService:AccountHttpService,
-    private _snackBar:UtilityService
+    private _snackBar:UtilityService,
+    private _route:Router
     ) { }
 
   password = new FormControl('',
@@ -46,13 +48,14 @@ export class ChangePasswordComponent implements OnInit {
         password:this.password.value,
         confirm_password:this.confirm_password.value
       }
-      console.log(this.data)
+      // console.log(this.data)
       this._httpService.changePassword(this.data)
       .subscribe(
         response => {
           if(response['code']===200){
-            this._snackBar.snackBarMessage("Password changed !")
-            localStorage.setItem('token',response['token'])
+            localStorage.removeItem('token')
+            this._route.navigate(['login'])
+            this._snackBar.snackBarMessage("Password changed. Please login again to continuoue.")
           }else{
             this._snackBar.snackBarMessage(response['msg'])
           }
@@ -64,6 +67,9 @@ export class ChangePasswordComponent implements OnInit {
     }
   }
 
+  notes(){
+    this._route.navigate([''])
+  }
   ngOnInit() {
   }
 
